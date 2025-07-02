d0dc3b791e92cabf445e3b5324d883cd705ac00a
Bernard-Kuria <bernard.i.kuria@gmail.com>
2025-07-02 15:02:02 +0300

    first commit

---
+++ routes/auth.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const authRouter = Router();
+
+authRouter.post("/sign-up", (req, res) => res.send({ tittle: "Sign up" }));
+authRouter.post("/sign-in", (req, res) => res.send({ tittle: "Sign in" }));
+authRouter.post("/sign-out", (req, res) => res.send({ tittle: "Sign out" }));
+
+export default authRouter;
